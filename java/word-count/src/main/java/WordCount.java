04230bce087efd28e91e667bf34db4f3950afec1
Johan Bsson <johan.bsson@gmail.com>
2017-02-03 16:56:21 +0100

    moved som files

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -5,11 +5,11 @@ import java.util.Map;
 public class WordCount {
     public Map<String,Integer> phrase(String word) {
         Map<String, Integer> wordMap;
-        wordMap = new HashMap<>();
+        wordMap = new HashMap<String, Integer>();
         //todo split string
         System.out.println("Phrase is:: " + word);
         String[] wordArray;
-        wordArray = " ".split(word);
+        wordArray = word.split(" ");
         System.out.println("wordArray is: " + wordArray.toString());
 
         //todo interate over string
@@ -17,7 +17,15 @@ public class WordCount {
         for(String w : wordArray){
             System.out.println("w in loop is" + w);
             //todo check if string is in map
-            if(wordMap.get(w) == -1){
+            if(wordMap.isEmpty()){
+                System.out.println("wordmap length" + wordMap.size());
+                System.out.println("inside is empty");
+                wordMap.put(w, 1);
+                System.out.println("wordmap length" + wordMap.size());
+            }
+            System.out.println("wordmapentry" + wordMap.get(w));
+            /*
+            else if(wordMap.get(w) == -1){
                 //todo if not add it with coint of 1
                 wordMap.put(w, 1);
             }
@@ -25,6 +33,7 @@ public class WordCount {
                 //todo if it is present, increas count
                 wordMap.put(w, wordMap.get(w) + 1);
             }
+            */
 
 
 
