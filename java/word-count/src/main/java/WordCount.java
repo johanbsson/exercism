689fab5fe99b38f504a7dc52d600fb12912f8094
Johan Börjesson <johan@infomega.se>
2017-02-04 06:23:41 +0100

    Done something i wordcount

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -5,21 +5,28 @@ import java.util.Map;
 public class WordCount {
     public Map<String,Integer> phrase(String word) {
         Map<String, Integer> wordMap;
-        wordMap = new HashMap<>();
+        wordMap = new HashMap<String, Integer>();
         //todo split string
-        System.out.println("Phrase is:: " + word);
         String[] wordArray;
         wordArray = " ".split(word);
-        System.out.println("wordArray is: " + wordArray.toString());
+        System.out.println("wordArray is here " + wordArray);
 
         //todo interate over string
 
         for(String w : wordArray){
-            System.out.println("w in loop is" + w);
+            //fixme It seems like variable w is empty here
+            //todo find out how to iterate over  word array
+            //todo maybe it's the string thats empty in testcase that causes problem
+            System.out.println("w is this in for loop " + w);
             //todo check if string is in map
-            if(wordMap.get(w) == -1){
+            if(wordMap.get(w) == null){
                 //todo if not add it with coint of 1
+                System.out.println("w is this in for loop " + w);
+
+                System.out.println("before w is " + w);
                 wordMap.put(w, 1);
+                System.out.println("after w is " + w);
+                System.out.println("Wordmap after init of word " + wordMap);
             }
             else {
                 //todo if it is present, increas count
