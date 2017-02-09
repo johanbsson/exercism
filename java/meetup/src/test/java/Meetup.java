cff7457b12fc6ae4e1daae3e46c99d28092326df
Johan Börjesson <johan@infomega.se>
2017-02-09 10:24:09 +0100

    Started to work on java meetup

---
+++ java/meetup/src/test/java/Meetup.java
@@ -0,0 +1,15 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+/**
+ * Created by johan on 2017-02-09.
+ */
+public class Meetup {
+    public Meetup(int i, int i1) {
+    }
+
+    public LocalDate day(DayOfWeek monday, MeetupSchedule teenth) {
+
+        return LocalDate.of(2013, 5, 13);
+    }
+}
