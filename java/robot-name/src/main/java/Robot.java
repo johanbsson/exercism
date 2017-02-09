88b6d509a9cbab1e79b0a368c5959f02b52211b9
Johan Börjesson <johan@infomega.se>
2017-02-09 09:05:07 +0100

    added some missing files to robot name

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -6,9 +6,6 @@ public class Robot {
     public Robot(){
         generateName();
     }
-    public String getName(){
-        return name;
-    }
 
     public void reset() {
     }
--- java/robot-name/src/main/java/RobotExp.java
+++ java/robot-name/src/main/java/RobotExp.java
@@ -7,5 +7,11 @@ public class RobotExp {
     public static void main(String[] args) {
         Random r = new Random();
         System.out.println(r.nextInt(2));
+        char a = 'A';
+        char b = 'B';
+        int inta = a;
+        int intb = b;
+
+        System.out.println("A is:" + inta + " and B is:" + intb);
     }
 }
