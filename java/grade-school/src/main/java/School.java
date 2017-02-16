bffc52258afa6322cee5cd87c5c1c46b25604ec5
Johan Börjesson <johan@infomega.se>
2017-02-16 15:16:54 +0100

    Started to work on grade-scool. Doesn't build yet!

---
+++ java/grade-school/src/main/java/School.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+/**
+ * Created by johan on 2017-02-16.
+ */
+public class School {
+    public <T> T numberOfStudents() {
+    }
+
+    public void add(String aimee, int i) {
+    }
+
+    public <T> T grade(int i) {
+
+    }
+
+    public Map studentsByGradeAlphabetical() {
+    }
+}
