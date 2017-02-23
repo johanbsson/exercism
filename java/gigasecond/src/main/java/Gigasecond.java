f9d1ec75d63733b869592a83420338d22048777e
Johan Börjesson <johan@infomega.se>
2017-02-23 14:46:35 +0100

    troligen värsta konflikten på gigasecond med nucen

---
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Created by johan on 2017-02-20.
+ */
+public class Gigasecond {
+    public Gigasecond(LocalDate localDate) {
+    }
+
+    public Gigasecond(LocalDateTime localDateTime) {
+    }
+
+    public LocalDateTime getDate() {
+        LocalDateTime ret = null;
+        return ret;
+    }
+}
