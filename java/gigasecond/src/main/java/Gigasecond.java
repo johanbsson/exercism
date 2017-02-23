e57c136424bc4eb5f8395d627d2e7d839750fac8
Johan Bsson <johan.bsson@gmail.com>
2017-02-23 14:42:00 +0100

    jobbat med gigasecond på nuc

---
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -0,0 +1,22 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Created by johan on 2017-02-18.
+ */
+public class Gigasecond {
+    long epochBirthTime;
+    public Gigasecond(LocalDate date) {
+        this.epochBirthTime  = Instant.from(date).getEpochSecond();
+    }
+
+    public Gigasecond(LocalDateTime dateTime) {
+        epochBirthTime  = Instant.from(dateTime).getEpochSecond();
+    }
+
+    public LocalDateTime getDate() {
+        Instant i = Instant.ofEpochSecond(epochBirthTime + 1000000000);
+        return LocalDateTime.ofInstant(i);
+    }
+}
